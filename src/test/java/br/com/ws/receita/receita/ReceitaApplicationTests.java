cb43a41ea87d394d3e95e70e24a05941f3ef873e
BrunoElias <bruno-150@hotmail.com>
2022-08-08 21:59:18 -0300

    Criacao Pagina Login

---
+++ src/test/java/br/com/ws/receita/receita/ReceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ws.receita.receita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
